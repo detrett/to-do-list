a1a1d8d5e576b39a201fcf3469134bd4c4d406fa
Alan Blanco <66321582+detrett@users.noreply.github.com>
2024-09-28 15:22:57 +0200

    feat: add basic layout for task + collapsible func

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.css";
+
+const coll = document.querySelectorAll(".collapsible");
+
+coll.forEach((button, index) => {
+  button.addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    const content = document.getElementById(`details-content-${index}`);
+    
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+});
