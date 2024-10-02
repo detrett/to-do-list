5da6b3ef2978cd98214707162f41d4de97393f7d
Alan Blanco <66321582+detrett@users.noreply.github.com>
2024-10-02 14:37:40 +0200

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// webpack.config.js
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -29,10 +39,5 @@ module.exports = {
                 type: "asset/resource",
             },
         ],
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
     },
 };
